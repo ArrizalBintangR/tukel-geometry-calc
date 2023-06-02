43ef17855101392945a20ab35e96028ec15789e7
1ndr4123 <99191064+1ndr4123@users.noreply.github.com>
2023-06-02 15:17:23 +0700

    proses ditambahkan
    tinggal rapihin

--- input.php
+++ input.php
@@ -30,120 +30,88 @@
             break;
 
         default:
-            echo "<title>ARRIZAL GANTENKZ PALID NO DEBAD</title>";
+            echo "<title>COBLOS PAK MAYADI, HIDUP PAK MAYADI</title>";
             break;
     }
     ?>
 </head>
 
 <body>
-    <a href="index.php">BACK TO MAIN PAGE</a>
     <div class="container">
+        <a href="index.php">BACK TO MAIN PAGE</a>
         <div>
             <?php switch ($bentuk):
                 case 1: ?>
                     <label>Lingkaran</label>
-                    <form method="post">
+                    <form method="post" action="proses.php">
                         <label>jari-jari</label>
                         <input type="text" name="jari2" />
                         <br>
-                        <input type="submit" name="count" value="count" />
+                        <input type="hidden" name="bentuk" value="1">
+                        <input type="submit" name="count" value="Count" />
                     </form>
-                    <br>
-                    <label>BERIKUT LUAS LINGKARAN YANG ANDA CARI</label>
-                    <?php
-                    if (isset($_POST['count'])) {
-                        // echo "This is Button1 that is selected";
-                    }
-                    ?>
                     <?php break; ?>
                 <?php
                 case 2: ?>
-        </div>
-        <label>SEGITIGA SIKU-SIKU</label>
-        <form method="post">
-            <label>ALAS</label>
-            <input type="text" name="alas" />
-            <br>
-            <label for="">TINGGI</label>
-            <input type="text" name="tinggi" />
-            <br>
-            <input type="submit" name="count" value="count" />
-        </form>
-        <br>
-        <label>BERIKUT LUAS YANG ANDA CARI</label>
-        <?php
-                    if (isset($_POST['count'])) {
-                        // echo "This is Button1 that is selected";
-                    }
-        ?>
-        <div>
-            <?php break; ?>
-        <?php
+                    <label>SEGITIGA SIKU-SIKU</label>
+                    <form method="post" action="proses.php">
+                        <label>ALAS</label>
+                        <input type="text" name="alas" />
+                        <br>
+                        <label for="">TINGGI</label>
+                        <input type="text" name="tinggi" />
+                        <br>
+                        <input type="hidden" name="bentuk" value="2">
+                        <input type="submit" name="count" value="Count" />
+                    </form>
+                    <?php break; ?>
+                <?php
                 case 3: ?>
-        </div>
-        <label>BUJUR SANGKAR</label>
-        <form method="post">
-            <label>SISI</label>
-            <input type="text" name="sisi" />
-            <br>
-            <input type="submit" name="count" value="count" />
-        </form>
-        <br>
-        <label>BERIKUT LUAS YANG ANDA CARI</label>
-        <?php
-                    if (isset($_POST['count'])) {
-                        // echo "This is Button1 that is selected";
-                    }
-        ?>
-        <div>
-            <?php break; ?>
-        <?php
+                    <label>BUJUR SANGKAR</label>
+                    <form method="post" action="proses.php">
+                        <label>SISI</label>
+                        <input type="text" name="sisi" />
+                        <br>
+                        <input type="hidden" name="bentuk" value="3">
+                        <input type="submit" name="count" value="Count" />
+                    </form>
+                    <?php break; ?>
+                <?php
                 case 4: ?>
-        </div>
-        <label>PERSEGI PANJANG</label>
-        <form method="post">
-            <label>PANJANG</label>
-            <input type="text" name="panjang" />
-            <br>
-            <label for="">LEBAR</label>
-            <input type="text" name="lebar" />
-            <br>
-            <input type="submit" name="count" value="count" />
-        </form>
-        <br>
-        <label>BERIKUT LUAS YANG ANDA CARI</label>
-        <?php
-                    if (isset($_POST['count'])) {
-                        // echo "This is Button1 that is selected";
-                    }
-        ?>
-        <div>
-            <?php break; ?>
-        <?php
+                    <label>PERSEGI PANJANG</label>
+                    <form method="post" action="proses.php">
+                        <label>PANJANG</label>
+                        <input type="text" name="panjang" />
+                        <br>
+                        <label for="">LEBAR</label>
+                        <input type="text" name="lebar" />
+                        <br>
+                        <input type="hidden" name="bentuk" value="4">
+                        <input type="submit" name="count" value="Count" />
+                    </form>
+                    <?php break; ?>
+                <?php
                 case 5: ?>
+                    <label>JAJARAN GENJANG</label>
+                    <form method="post" action="proses.php">
+                        <label>ALAS</label>
+                        <input type="text" name="alas" />
+                        <br>
+                        <label for="">TINGGI</label>
+                        <input type="text" name="tinggi" />
+                        <br>
+                        <input type="hidden" name="bentuk" value="5">
+                        <input type="submit" name="count" value="Count" />
+                    </form>
+                    <?php break; ?>
+            <?php endswitch; ?>
         </div>
-        <label>JAJARAN GENJANG</label>
-        <form method="post">
-            <label>ALAS</label>
-            <input type="text" name="ALAS" />
-            <br>
-            <label for="">TINGGI</label>
-            <input type="text" name="TINGGI" />
-            <br>
-            <input type="submit" name="count" value="count" />
-        </form>
-        <br>
-        <label>BERIKUT LUAS YANG ANDA CARI</label>
-        <?php
-                    if (isset($_POST['count'])) {
-                        // echo "This is Button1 that is selected";
-                    }
-        ?>
         <div>
-            <?php break; ?>
-    <?php endswitch; ?>
+            <?php if (isset($luas)) : ?>
+                <p>Luas: <?php echo $luas; ?></p>
+            <?php endif; ?>
         </div>
+
     </div>
 </body>
 
--- proses.php
+++ proses.php
@@ -1,5 +1,68 @@
 <?php
-function HitungLuasLingkaran() {
+if (isset($_POST['count'])) {
+    $bentuk = $_POST['bentuk'];
+
+    switch ($bentuk) {
+        case 1: // Lingkaran
+            $jari2 = $_POST['jari2'];
+            $luas = hitungLuasLingkaran($jari2);
+            echo "Luas Lingkaran: " . $luas;
+            break;
+
+        case 2: // Segitiga Siku-Siku
+            $alas = $_POST['alas'];
+            $tinggi = $_POST['tinggi'];
+            $luas = hitungLuasSegitiga($alas, $tinggi);
+            echo "Luas Segitiga Siku-Siku: " . $luas;
+            break;
+
+        case 3: // Bujur Sangkar
+            $sisi = $_POST['sisi'];
+            $luas = hitungLuasPersegi($sisi);
+            echo "Luas Bujur Sangkar: " . $luas;
+            break;
+
+        case 4: // Persegi Panjang
+            $panjang = $_POST['panjang'];
+            $lebar = $_POST['lebar'];
+            $luas = hitungLuasPersegiPanjang($panjang, $lebar);
+            echo "Luas Persegi Panjang: " . $luas;
+            break;
+
+        case 5: // Jajaran Genjang
+            $alas = $_POST['alas'];
+            $tinggi = $_POST['tinggi'];
+            $luas = hitungLuasJajaranGenjang($alas, $tinggi);
+            echo "Luas Jajaran Genjang: " . $luas;
+            break;
+
+        default:
+            echo "Bentuk tidak valid";
+            break;
+    }
 }
 
-?>
+function hitungLuasLingkaran($jari2)
+{
+    return 3.14 * $jari2 * $jari2;
+}
+
+function hitungLuasSegitiga($alas, $tinggi)
+{
+    return 0.5 * $alas * $tinggi;
+}
+
+function hitungLuasPersegi($sisi)
+{
+    return $sisi * $sisi;
+}
+
+function hitungLuasPersegiPanjang($panjang, $lebar)
+{
+    return $panjang * $lebar;
+}
+
+function hitungLuasJajaranGenjang($alas, $tinggi)
+{
+    return $alas * $tinggi;
+}
